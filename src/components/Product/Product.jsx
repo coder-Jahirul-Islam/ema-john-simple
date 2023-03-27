8b90cc2453a93a6b0cef4e6b23db3ea3e8bde48d
Md. Jahirul Islam <jahirulislam12283@gmail.com>
2023-03-28 02:44:54 +0600

    product component layout display data

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    console.log(props);
+    const {img,name,seller,quantity, price} =props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <h6>{name}</h6>
+
+        </div>
+    );
+};
+
+export default Product;
