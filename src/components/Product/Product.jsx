0b2e1729f7c901342f7cd5fda34468a9f7645037
Md. Jahirul Islam <jahirulislam12283@gmail.com>
2023-03-28 03:20:26 +0600

    style product cart, style debug button position

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,11 +3,17 @@ import './Product.css'
 
 const Product = (props) => {
     console.log(props);
-    const {img,name,seller,quantity, price} =props.product;
+    const {img,name,seller,ratings,quantity, price} =props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h6>{name}</h6>
+           <div className="product-info">
+           <h6 className='product-name'>{name}</h6>
+            <p>Price: ${price}</p>
+            <p>Manufacturer: {seller}</p>
+            <p>Rating: {ratings} Stars</p>
+           </div>
+           <button className='btn-cart'>Add to Cart</button>
 
         </div>
     );
